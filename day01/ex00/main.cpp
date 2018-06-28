15a4260a2869b858632cccbf40b18fcb36a3ae73
Tapabrata Dey <tapabratadey02@gmail.com>
2018-06-28 08:38:14 -0700

    ex00

--- day01/ex00/main.cpp
+++ day01/ex00/main.cpp
@@ -1,7 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tadey <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/28 08:29:20 by tadey             #+#    #+#             */
+/*   Updated: 2018/06/28 08:29:22 by tadey            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Pony.Class.hpp"
 
-Pony  *ponyOnTheHeap(std::string string) { return new Pony(string); }
-Pony  ponyOntheStack(std::string string) { return Pony(string); }
+Pony  *ponyOnTheHeap(std::string string) 
+{ 
+    return new Pony(string);
+}
+
+Pony  ponyOntheStack(std::string string) 
+{ 
+    return Pony(string);
+}
 
 int main()
 {
