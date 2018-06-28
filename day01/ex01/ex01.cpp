3f40bc10cb445fd0ad12ca0d35ef81bb1459b9dc
Tapabrata Dey <tapabratadey02@gmail.com>
2018-06-28 08:37:47 -0700

    ex01

--- day01/ex01/ex01.cpp
+++ day01/ex01/ex01.cpp
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tadey <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/28 08:36:44 by tadey             #+#    #+#             */
+/*   Updated: 2018/06/28 08:36:44 by tadey            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 void memoryLeak()
 {
-    std::string* panthere = new std::string("String panthere");
+    std::string *panthere = new std::string("String panthere");
     std::cout << *panthere << std::endl;
     delete panthere;
 }
